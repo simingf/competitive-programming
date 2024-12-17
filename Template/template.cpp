6a78adad47c01bf5c666da7220811bfe5440ecd9
Siming <SimingRox@gmail.com>
2024-12-17 13:21:46 -0500

    template updates

--- Template/template.cpp
+++ Template/template.cpp
@@ -1,38 +1,36 @@
-/*Siming's C++14 Template For CP*/
+/*Siming's C++14/17/20 Template For CP*/
 #include <bits/stdc++.h>
 using namespace std;
 /*Debug*/
+#define dbg(x) cout<<"DEBUG: "<<(#x)<<" = "<<(x)<<"\n";
 /*vector*/ template<typename T> ostream &operator<<(ostream &os, const vector<T> &v){os<<"{";for(int i=0;i<v.size();++i){os<<v[i];if(i!=v.size()-1)os<<", ";}os<<"}";return os;}
 /*set*/ template<typename T> ostream &operator<<(ostream &os, const set<T> &s){os<<"{";for(auto &it:s){os<<it;if(it!=*s.rbegin())os<<", ";}os<<"}";return os;}
 /*list*/ template<typename T> ostream &operator<<(ostream &os, const list<T> &l){os<<"{";for(auto &it:l){os<<it;if(it!=*l.rbegin())os<<", ";}os<<"}";return os;}
 /*pair*/ template<typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &p){os<<"(";os<<p.first<<", "<<p.second<<")";return os;}
 /*map*/ template<typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &m){cout<<"{";for(auto &it:m){os<<it;}os<<"}";return os;}
 /*umap*/ template<typename T, typename S> ostream &operator<<(ostream &os, const unordered_map<T, S> &m){cout<<"{";for(auto &it:m){os<<it;}os<<"}";return os;}
-#define dbg(x) cout<<"DEBUG: "<<(#x)<<" = "<<(x)<<"\n";
 /*IO*/
 /*vector*/ template<typename T> istream &operator>>(istream &is, vector<T> &v){for(auto &i:v){is>>i;}return is;}
 #define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define setd(n) cout.precision(n);cout<<fixed;
 #define endl "\n"
 /*Acronyms*/
-#define int long long int
+typedef long long int64;
 #define str string
-#define vc vector
-#define vi vc<int>
-#define pb push_back
+#define vec vector
 #define umap unordered_map
-#define mp make_pair
+#define PB push_back
+#define MP make_pair
 #define ff first
 #define ss second
-const int INF = 1e18;
-const int MOD = 1e9+7;
 /*Util*/
-#define asc(i,s,e) for(int i=(s);i<=(e);++i)
-#define dsc(i,s,e) for(int i=(s);i>=(e);--i)
-#define each(i,x) for(auto &i:x)
-#define sz(x) ((int)(x).size())
-#define all(x) begin(x), end(x)
-#define rall(x) rbegin(x), rend(x)
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define FOR(i,l,h) for(int i=(l);i<=(h);++i)
+#define FORD(i,h,l) for(int i=(h);i>=(l);--i)
+#define EACH(i,x) for(auto &i:x)
+#define SIZE(x) ((int)(x).size())
+#define ALL(x) begin(x), end(x)
+#define ALLD(x) rbegin(x), rend(x)
 #define gcd(a,b) __gcd(a,b)
 #define lcm(a,b) ((a)/__gcd(a,b)*(b))
 #define cdiv(a,b) ((a==0)?0:((a)-1)/(b)+1)
