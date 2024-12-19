90bef53382903b5c75a6844cfbf748f28ae591da
Siming <SimingRox@gmail.com>
2024-12-19 01:24:30 -0500

    move dbg function down a bit

--- sol.cpp
+++ sol.cpp
@@ -2,13 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 /*Debug*/
-#define dbg(...) [](const auto&... args) { string _s = #__VA_ARGS__; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); ((cout << "DBG: " << (_it++)->c_str() << " = " << args << "\n"), ...); }(__VA_ARGS__)
 /*vector*/ template<typename T> ostream &operator<<(ostream &os, const vector<T> &v){os<<"VEC[";for(int i=0;i<v.size();++i){os<<v[i];if(i!=v.size()-1)os<<", ";}os<<"]";return os;}
 /*list*/ template<typename T> ostream &operator<<(ostream &os, const list<T> &l){os<<"LIST[";for(auto &it:l){os<<it;if(it!=*l.rbegin())os<<", ";}os<<"]";return os;}
 /*set*/ template<typename T> ostream &operator<<(ostream &os, const set<T> &s){os<<"SET{";for(auto &it:s){os<<it;if(it!=*s.rbegin())os<<", ";}os<<"}";return os;}
 /*pair*/ template<typename T, typename S> ostream &operator<<(ostream &os, const pair<T, S> &p){os<<"("<<p.first<<":"<<p.second<<")";return os;}
 /*map*/ template<typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &m){os<<"MAP{";for(auto &it:m){os<<it;if(it!=*m.rbegin())os<<", ";}os<<"}";return os;}
 /*umap*/ template<typename T, typename S> ostream &operator<<(ostream &os, const unordered_map<T, S> &m){os<<"UMAP{";bool first=true;for(auto &it:m){if(!first)os<<", ";first=false;os<<it;}os<<"}";return os;}
+#define dbg(...) [](const auto&... args) { string _s = #__VA_ARGS__; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); ((cout << "DBG: " << (_it++)->c_str() << " = " << args << "\n"), ...); }(__VA_ARGS__)
 /*IO*/
 /*vector*/ template<typename T> istream &operator>>(istream &is, vector<T> &v){for(auto &i:v){is>>i;}return is;}
 #define fastio ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
@@ -42,7 +42,7 @@ using namespace std;
 
 void solve()
 {
-    
+
 }
 
 signed main()
