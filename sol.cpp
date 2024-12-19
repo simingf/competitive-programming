1226d311a8763b2febc7bf2c1b27efe89f7d41e5
Siming <SimingRox@gmail.com>
2024-12-19 02:58:04 -0500

    better hash tables

--- sol.cpp
+++ sol.cpp
@@ -14,11 +14,19 @@ using namespace std;
 #define endl "\n"
 /*Debug*/
 #define dbg(...) [](const auto&... args) { string _s = #__VA_ARGS__; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); ((cout << "DBG: " << (_it++)->c_str() << " = " << args << "\n"), ...); }(__VA_ARGS__)
+/*Better Hash Table*/
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {
+    int operator()(int x) const { return x ^ RANDOM; }
+};
+#define umap gp_hash_table
+// gp_hash_table<key, int, chash> table;
 /*Acronyms*/
 #define int long long
 #define str string
 #define vec vector
-#define umap unordered_map
 #define pb push_back
 #define mp make_pair
 #define ff first
