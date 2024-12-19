2c98140b8d78f14cf7f82d915ce6bf96a889d93d
Siming <SimingRox@gmail.com>
2024-12-19 17:01:54 -0500

    remove chash (only useful for codeforces)

--- Template/template.cpp
+++ Template/template.cpp
@@ -17,12 +17,7 @@ using namespace std;
 /*Better Hash Table*/
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
-const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash {
-    int operator()(int x) const { return x ^ RANDOM; }
-};
 #define umap gp_hash_table
-// gp_hash_table<key, int, chash> table;
 /*Acronyms*/
 #define int long long
 #define str string
